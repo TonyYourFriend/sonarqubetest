f6ade82e4ae7043931ff09987e4c7926d5e42605
Tony Dang <tony.dang@younium.com>
2024-05-30 11:12:20 +0200

    testpr5

--- MusicPlayerApp/Program.cs
+++ MusicPlayerApp/Program.cs
@@ -14,6 +14,16 @@ namespace MusicPlayerApp
         [STAThread]
         static void Main()
         {
+            int uselessint = 6;
+            uselessIInt = 6;
+            uselessIInt = 6;
+            for(int i= 0; i < 4; i++){
+                Console.WriteLine(i);
+            }
+            for(int i= 0; i < 4; i++){
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(uselessIInt);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MusicPlayerApp());
