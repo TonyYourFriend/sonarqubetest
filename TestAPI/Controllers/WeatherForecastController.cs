73cebbaaf66ebc141f493a7c91ae39a03e9ae6be
Tony Dang <tony.dang@younium.com>
2024-05-31 11:34:22 +0200

    update

--- TestAPI/Controllers/WeatherForecastController.cs
+++ TestAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestAPI.Controllers
     {
         public static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Milder", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
