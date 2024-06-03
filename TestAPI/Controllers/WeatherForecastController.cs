5c62723310344711fe8c3dca469df833862a18d0
Tony Dang <tony.dang@younium.com>
2024-06-03 09:44:43 +0200

    testupdatea

--- TestAPI/Controllers/WeatherForecastController.cs
+++ TestAPI/Controllers/WeatherForecastController.cs
@@ -21,6 +21,14 @@ namespace TestAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Array X = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+            Console.WriteLine(X);
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
